d132f41420561939b0bd939e70a53d302e4038f8
Olga Cortez <olgalataviacortez@gmail.com>
2019-10-18 10:56:57 -0500

    Met MVP

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import axios from "axios";
+import axiosWithAuth from '../utils/axiosWithAuth';
 
 const initialColor = {
   color: "",
@@ -21,10 +21,26 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
-  };
+    axiosWithAuth()
+    .put(`/colors/${colorToEdit.id}`, colorToEdit)
+    .then(res => {
+      updateColors([
+      ...colors.filter(color => color.id !== colorToEdit.id),
+      res.data,
+    ])
+    setEditing(false);
+  })
+  .catch(err => console.log(err.response));
+};
 
-  const deleteColor = color => {
+  const deleteColor = colorToDelete => {
     // make a delete request to delete this color
+    axiosWithAuth()
+    .delete(`/colors/${colorToDelete.id}`)
+    .then(res => {
+      updateColors(colors.filter(color => color.id !== colorToDelete.id));
+    })
+    .catch(err => console.log(err.response));
   };
 
   return (
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,63 @@
 import React from "react";
+import axiosWithAuth from '../utils/axiosWithAuth';
 
-const Login = () => {
+class Login extends React.Component {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  state = {
+    credentials: {
+      username: '',
+      password: ''
+    }
+  };
+
+  handleChanges = e => {
+    this.setState({
+        credentials: {
+            ...this.state.credentials,
+            [e.target.name]: e.target.value
+        }
+    });
+};
+
+  login = e => {
+    e.preventDefault();
+    axiosWithAuth()
+    .post('/login', this.state.credentials)
+    .then(res => {
+        localStorage.setItem('token', res.data.payload);
+        this.props.history.push('/protected');
+    })
+    .catch(err => console.log(err.response));
+};
+  
+  render() {
   return (
-    <>
+     <div>
       <h1>Welcome to the Bubble App!</h1>
       <p>Build a login page here</p>
-    </>
-  );
-};
+     <form onSubmit={this.login}>
+         <input
+         type="text"
+         name="username"
+         placeholder="Username"
+         value={this.state.credentials.username}
+         onChange={this.handleChanges}
+         />
+
+         <input
+         type="password"
+         name="password"
+         placeholder="Password"
+         value={this.state.credentials.password}
+         onChange={this.handleChanges}
+         />
+
+         <button>Log In</button>
+     </form>
+ </div>
+    );
+  }
+}
 
 export default Login;
